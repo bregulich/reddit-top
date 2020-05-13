a8166762a1c934e4304bae87dbe216d37896da07
Bogdan Regulich <bogdanregulich@live.com>
2020-05-13 19:40:25 +0300

    Repository

--- app/src/main/java/com/bokugan/reddittop/repository/PostRepository.kt
+++ app/src/main/java/com/bokugan/reddittop/repository/PostRepository.kt
@@ -5,34 +5,33 @@ import com.bokugan.reddittop.datasource.LocalPostDataSourceProvider
 import com.bokugan.reddittop.datasource.RemotePostDataSource
 import com.bokugan.reddittop.datasource.RemotePostDataSourceProvider
 import com.bokugan.reddittop.room.Post
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 interface PostRepository {
-    val posts: Flow<List<Post>>
+    suspend fun fetchPosts(): Flow<List<Post>>
     suspend fun updatePosts(posts: List<Post>)
 }
 
 private class PostRepositoryService(
-    private val coroutineScope: CoroutineScope,
     private val localDataSource: LocalPostDataSource,
     private val remoteDataSource: RemotePostDataSource
 ) : PostRepository {
 
-    override val posts: Flow<List<Post>>
-        get() {
-            coroutineScope.launch { updatePosts(remoteDataSource.fetchPosts()) }
-            return localDataSource.posts
+    override suspend fun fetchPosts(): Flow<List<Post>> {
+        coroutineScope {
+            launch { updatePosts(remoteDataSource.fetchPosts()) }
         }
+        return localDataSource.posts
+    }
 
-    override suspend fun updatePosts(posts: List<Post>) = localDataSource.updatePosts(posts)
+    override suspend fun updatePosts(posts: List<Post>) =
+        localDataSource.updatePosts(posts)
 }
 
 private val PostRepositoryInstance by lazy {
     PostRepositoryService(
-        GlobalScope,
         LocalPostDataSourceProvider.provide(),
         RemotePostDataSourceProvider.provide()
     )
