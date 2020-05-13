b50bc5ecfabdfc8d178673314b97d6af7b1e80e3
Bogdan Regulich <bogdanregulich@live.com>
2020-05-13 18:18:04 +0300

    Repository

--- app/src/main/java/com/bokugan/reddittop/room/PostDao.kt
+++ app/src/main/java/com/bokugan/reddittop/room/PostDao.kt
@@ -3,10 +3,12 @@ package com.bokugan.reddittop.room
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PostDao {
+    @Query("SELECT * FROM post")
     fun getAll(): Flow<List<Post>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
